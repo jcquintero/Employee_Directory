a1b4b203b64722c46713349f76593bf0b122a2d9
JUAN CARLOS QUINTERO VALEJO <jcquinterov@gmail.com>
2022-05-13 16:55:59 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+![Startup employee directory screenshot](https://res.cloudinary.com/dqe0hw0ru/image/upload/v1565109431/startup.png)
+
+# 🔎 Employee Directory
+### Employee directory project based on the [Treehouse Front-End Techdegree](https://join.teamtreehouse.com/techdegree/)
+
+## Project Overview:
+
+In this project, I was challenged with using the Random User Generator API (https://randomuser.me/) to grab information for 12 random “employees,” and using the data to build an Awesome Startup employee directory. 
+
+I requested a JSON object from the API using fetch and parsed the data so that 12 employees are listed in a grid with their thumbnail image, full name, email, and location. Clicking the employee’s entry opens a modal window with more detailed information such as the employee’s birthday and address.
+
+## Tools/skills used:
+* HTML5
+* CSS Grid
+* Responsive Design
+* JavaScript
+* Fetch
+
+#### I originally created this project for my front-end Techdegree, but I was never proud of the code I wrote. I've removed my original jQuery and added CSS grid, and now I'm a little more happy with the result. Perfectionist? Me? Never! 😉
+
+## See live demo: [Employee directory project](https://nicolepdev.github.io/startup-employee-directory/)
---
+++ app.js
@@ -0,0 +1,117 @@
+// GLOBAL VARIABLES
+
+let employeeList = [];
+
+const apiURL = `https://randomuser.me/api/?results=12&nat=us&inc=name,email, phone,location,picture,cell,dob`;
+//const apiURL = `http://tweb.eafit.edu.co/PSIGW/PeopleSoftServiceListeningConnector/PSFT_CS/EA_EBS_PERS_PS.1.wsdl, phone,location,picture,cell,dob`;
+const directoryContainer = document.querySelector('.directory-container');
+const searchInput = document.querySelector('.search-input');
+const overlay = document.querySelector('.overlay');
+const modalText = document.querySelector('.modal-content');
+const modalClose = document.querySelector('.modal-x-closer');
+
+// FETCH DATA FROM API
+
+fetch(apiURL)
+  .then(response => response.json())
+  .then(response => response.results)
+  .then(displayEmployees)
+  .catch(
+    error =>
+      (directoryContainer.innerHTML = `Sorry, there has been an error pulling data from the API`)
+  );
+
+// DISPLAY EMPLOYEE INFORMATION
+
+function displayEmployees(employeeData) {
+  employees = employeeData;
+  // STORE EMPLOYEE HTML
+  let employeeHTML = ``;
+  // LOOP THROUGH EACH EMPLOYEE AND CREATE MARKUP
+  employees.forEach((employee, i) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let state = employee.location.state;
+    let picture = employee.picture;
+
+    employeeHTML += `
+    <div class="directory-container__card" data-index="${i}">
+        <img src="${picture.large}" alt="${name.first} ${
+      name.last
+    }" class="card__img" />
+        <div class="card__info">
+          <h2 class="card__info-name">${name.first} ${name.last}</h2>
+          <p class="card__info-email">${email}</p>
+          <p class="card__info-address">${city}, ${state}</p>
+        </div>
+      </div>
+    `;
+  });
+  directoryContainer.innerHTML = employeeHTML;
+}
+
+// EMPLOYEE SEARCH
+
+searchInput.addEventListener('keyup', function(e) {
+  let employeeCards = document.querySelectorAll('.directory-container__card');
+  let currentQuery = this.value;
+
+  for (let i = 0; i < employeeCards.length; i++) {
+    if (
+      !employeeCards[i].innerText
+        .toUpperCase()
+        .includes(currentQuery.toUpperCase())
+    ) {
+      employeeCards[i].style.display = 'none';
+    } else {
+      employeeCards[i].style.display = 'flex';
+    }
+  }
+});
+
+// EMPLOYEE MODALS
+
+function modalDisplay(index) {
+  let {
+    name,
+    dob,
+    phone,
+    email,
+    location: { city, street, state, postcode },
+    picture
+  } = employees[index];
+
+  let date = new Date(dob.date);
+
+  const modalContent = `
+  <div class="modal-individual data-index="${index}">
+        <img src="${picture.large}" alt="${name.first} ${
+    name.last
+  }" class="card__img-modal" />
+        <div>
+          <h2 class="card__info-name">${name.first} ${name.last}</h2>
+          <p class="card__info-email">${email}</p>
+          <hr />
+          <p>${phone}</p>
+          <p class="card__info-address">${street} <br/>${city}, ${state} ${postcode}</p>
+          <p><strong>Birthday:</strong> ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+      </div>
+  `;
+  overlay.classList.remove('modal-hide');
+  modalText.innerHTML = modalContent;
+}
+
+directoryContainer.addEventListener('click', e => {
+  if (e.target !== directoryContainer) {
+    const card = e.target.closest('.directory-container__card');
+    const index = card.getAttribute('data-index');
+
+    modalDisplay(index);
+  }
+});
+
+modalClose.addEventListener('click', () => {
+  overlay.classList.add('modal-hide');
+});
